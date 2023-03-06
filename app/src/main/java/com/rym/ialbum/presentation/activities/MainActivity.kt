aedede2cf147086f61cec9fc882c466b6f268d86
RymaDa <109346721+RymaDa@users.noreply.github.com>
2023-03-06 14:16:00 +0100

    Project Skeleton Setup
    I am based on clean architecture and MVVM architecture

--- app/src/main/java/com/rym/ialbum/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/rym/ialbum/presentation/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.rym.ialbum
+package com.rym.ialbum.presentation.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rym.ialbum.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
